889e8d6425cb69d964743dbfa5eb04a9c3a40b6a
bin6675 <kwb6675@gmail.com>
2020-05-11 05:35:45 +0900

    20200511

--- app/src/main/java/com/projectHandClap/youruniv/CalendarActivity.java
+++ app/src/main/java/com/projectHandClap/youruniv/CalendarActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CalenderActivity extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calender);
+        setContentView(R.layout.activity_calendar);
     }
 }
--- app/src/main/java/com/projectHandClap/youruniv/MainActivity.java
+++ app/src/main/java/com/projectHandClap/youruniv/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private  DrawerLayout drawerLayout;
     private View drawerView;
 
@@ -20,101 +21,66 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         drawerView = (View)findViewById(R.id.drawer);
-        drawerLayout.addDrawerListener(listener);
 
-        // drawer 열기
-        Button btn_open_drawer = (Button)findViewById(R.id.open_drawer);
-        btn_open_drawer.setOnClickListener(new View.OnClickListener() {
+        Button open_recorder = (Button)findViewById(R.id.open_recorder);
+        Button open_calendar = (Button)findViewById(R.id.open_calendar);
+        Button open_gallery = (Button)findViewById(R.id.open_gallery);
+        Button close_drawer = (Button)findViewById(R.id.close_drawer);
+        Button open_drawer = (Button)findViewById(R.id.open_drawer);
+        Button open_memo = (Button)findViewById(R.id.open_memo);
+        Button open_set = (Button)findViewById(R.id.open_set);
+
+        View.OnClickListener listener = new View.OnClickListener()
+        {
             @Override
             public void onClick(View view) {
-                drawerLayout.openDrawer(drawerView);
-            }
-        });
+                switch (view.getId())
+                {
+                    case R.id.open_drawer:
+                        drawerLayout.openDrawer(drawerView);
+                        break;
 
-        // drawer 닫기
-        Button btn_close = (Button)findViewById(R.id.close_drawer);
-        btn_close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.closeDrawers();
-            }
-        });
+                    case R.id.close_drawer:
+                        drawerLayout.closeDrawer(drawerView);
+                        break;
 
-        // set 열기
-        Button btn_set = (Button)findViewById(R.id.open_set);
-        btn_set.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, SetActivity.class);
-                startActivity(intent);
-            }
-        });
+                    case R.id.open_set:
+                        Intent intent_set = new Intent(MainActivity.this, SetActivity.class);
+                        startActivity(intent_set);
+                        break;
 
-        // schedule 열기
-        Button btn_schedule = (Button)findViewById(R.id.open_schedule);
-        btn_schedule.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-            }
-        });
+                    case R.id.open_schedule:
+                        break;
 
-        // gallery 열기
-        Button btn_gallery = (Button)findViewById(R.id.open_gallery);
-        btn_gallery.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, SetActivity.class);
-                startActivity(intent);
-            }
-        });
+                    case R.id.open_recorder:
+                        Intent intent_recorder = new Intent(MainActivity.this, RecorderActivity.class);
+                        startActivity(intent_recorder);
+                        break;
 
-        // recorder 열기
-        Button btn_recorder = (Button)findViewById(R.id.open_recorder);
-        btn_recorder.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, SetActivity.class);
-                startActivity(intent);
-            }
-        });
+                    case R.id.open_memo:
+                        Intent intent_memo = new Intent(MainActivity.this, MemoActivity.class);
+                        startActivity(intent_memo);
+                        break;
 
-        // memo 열기
-        Button btn_memo = (Button)findViewById(R.id.open_memo);
-        btn_memo.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, SetActivity.class);
-                startActivity(intent);
-            }
-        });
+                    case R.id.open_gallery:
+                        Intent intent_gallery = new Intent(MainActivity.this, GalleryActivity.class);
+                        startActivity(intent_gallery);
+                        break;
 
-        // calendar 열기
-        Button btn_calendar = (Button)findViewById(R.id.open_calendar);
-        btn_calendar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, SetActivity.class);
-                startActivity(intent);
+                    case R.id.open_calendar:
+                        Intent intent_calendar = new Intent(MainActivity.this, CalendarActivity.class);
+                        startActivity(intent_calendar);
+                        break;
+                }
             }
-        });
+        };
+
+        open_calendar.setOnClickListener(listener);
+        open_recorder.setOnClickListener(listener);
+        open_gallery.setOnClickListener(listener);
+        close_drawer.setOnClickListener(listener);
+        open_drawer.setOnClickListener(listener);
+        open_memo.setOnClickListener(listener);
+        open_set.setOnClickListener(listener);
     }
-
-    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener()
-    {
-        @Override
-        public void onDrawerClosed(@NonNull View view) {
-        }
-
-        @Override
-        public void onDrawerStateChanged(int newState) {
-        }
-
-        @Override
-        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
-        }
-
-        @Override
-        public void onDrawerOpened(@NonNull View view) {
-        }
-    };
 }

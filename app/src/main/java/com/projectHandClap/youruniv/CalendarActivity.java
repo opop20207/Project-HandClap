c8b3f46e8a4ca497f96fb623a5dc8d847d93636c
bin6675 <kwb6675@gmail.com>
2020-05-17 16:38:23 +0900

    20200517_activity_calendar

--- app/src/main/java/com/projectHandClap/youruniv/CalendarActivity.java
+++ app/src/main/java/com/projectHandClap/youruniv/CalendarActivity.java
@@ -2,7 +2,12 @@ package com.projectHandClap.youruniv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        FloatingActionButton fab = findViewById(R.id.add_calendar);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CalendarActivity.this, AddCalendarActivity.class));
+            }
+        });
+
+        ListView schedule_list = findViewById(R.id.schedule_list);
     }
 }

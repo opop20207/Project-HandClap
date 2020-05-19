dc8a7df1c415076b1b6a9f88a4457f9e0ed36e60
opop20207 <opop202077@gmail.com>
2020-05-20 00:11:17 +0900

    0519 setting

--- app/src/main/java/com/projectHandClap/youruniv/SetActivity.java
+++ app/src/main/java/com/projectHandClap/youruniv/SetActivity.java
@@ -3,6 +3,11 @@ package com.projectHandClap.youruniv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SetActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class SetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
+
+        init();
+    }
+
+    public void init(){
+        DatabaseHelper db = new DatabaseHelper(getApplicationContext());
+        ArrayList<TimetableData> t = db.getTimetable();
+        final List<String> listItems = new ArrayList<>();
+        for(TimetableData t1 : t){
+            listItems.add(t1.timetable_title);
+        }
+
+        TextView txv_setting_main_timetable_id = (TextView)findViewById(R.id.txv_setting_main_timetable_id);
+        txv_setting_main_timetable_id.setText("1");
+    }
+
+    public void onClickSetting(View v){
+        switch(v.getId()){
+            case R.id.btn_setting_submit:
+                break;
+            case R.id.btn_setting_main_timetable_id:
+                break;
+        }
     }
 }

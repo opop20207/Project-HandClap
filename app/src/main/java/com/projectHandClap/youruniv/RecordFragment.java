7a51388ece82c381edaf943c138e71557c126765
seongjinah <5829341@naver.com>
2020-05-27 09:09:46 +0900

    Recorder

---
+++ app/src/main/java/com/projectHandClap/youruniv/RecordFragment.java
@@ -0,0 +1,176 @@
+package com.projectHandClap.youruniv;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RecordFragment extends Fragment implements View.OnClickListener {
+
+    private NavController navController;
+    private ImageView listBtn, recordBtn;
+    private TextView fildnameText;
+
+    private  boolean isRecording = false;
+
+    private String recordPermission = Manifest.permission.RECORD_AUDIO;
+    private int PERMISSION_CODE = 21;
+
+    private MediaRecorder mediaRecorder;
+    private String recordFile;
+
+    private Chronometer timer;
+
+    public RecordFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_record, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        listBtn = view.findViewById(R.id.record_list_btn);
+        recordBtn = view.findViewById(R.id.record_btn);
+        timer = view.findViewById(R.id.record_timer);
+        fildnameText = view.findViewById(R.id.record_filename);
+
+        listBtn.setOnClickListener(this);
+        recordBtn.setOnClickListener(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.record_list_btn:
+                if(isRecording){
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext());
+                    alertDialog.setPositiveButton("OKAY", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            navController.navigate(R.id.action_recordFragment_to_audioListFragment);
+                            isRecording=false;
+                        }
+                    });
+                    alertDialog.setNegativeButton("CANCEL", null);
+                    alertDialog.setTitle("Audio Still recording");
+                    alertDialog.setMessage("Are you sure, you want to stop the recording?");
+                    alertDialog.create().show();
+                } else {
+                    navController.navigate(R.id.action_recordFragment_to_audioListFragment);
+                }
+                break;
+
+            case R.id.record_btn:
+                if(isRecording) {
+                    //Stop Recording
+                    stopREcording();
+
+                    recordBtn.setImageDrawable(getResources().getDrawable(R.drawable.recordstopbtn, null));
+                    isRecording = false;
+                } else{
+                    //Start Recording
+                    if(checkPermission()) {
+                        startRecording();
+
+                        recordBtn.setImageDrawable(getResources().getDrawable(R.drawable.recordbtn, null));
+                        isRecording = true;
+                    }
+                }
+                break;
+        }
+    }
+
+    private void stopREcording() {
+        timer.stop();
+
+        fildnameText.setText("Recording Stoped, File Saved : " + recordFile);
+
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+    }
+
+    private void startRecording() {
+        timer.setBase(SystemClock.elapsedRealtime());
+        timer.start();
+
+        String recordPath = getActivity().getExternalFilesDir("/").getAbsolutePath();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd hh:mm", Locale.KOREA);
+        Date now = new Date();
+
+        recordFile = formatter.format(now) + ".3gp";
+        //recordFile = formatter.format(now) + recordTime + " "+ subjectName ".mp4";
+        //recordTime = time of record
+        //subjectName = name of recording subject
+
+        fildnameText.setText("Recording, File Name : \n" + recordFile);
+
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(recordPath+ "/" + recordFile);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaRecorder.start();
+
+    }
+
+    private boolean checkPermission() {
+        if(ActivityCompat.checkSelfPermission(getContext(), recordPermission) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        } else{
+            ActivityCompat.requestPermissions(getActivity(), new String[]{recordPermission}, PERMISSION_CODE);
+            return false;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(isRecording) {
+            stopREcording();
+        }
+    }
+}
